23dcdcc845e4c2a89c369ec9ff82f067545ee172
souj <soujanya.venkatesh@gmail.com>
2019-01-31 22:37:44 +0530

    Rev1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './src/intro/intro.component';
+import { GradeComponent } from './src/grade/grade.component';
+import { PolicyComponent } from './src/policy/policy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    GradeComponent,
+    PolicyComponent
   ],
   imports: [
     BrowserModule,
